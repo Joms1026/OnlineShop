53f25ccdad19fff222296f760e9d22038f6a1bfd
stefanie <stefanieangelina.sa@gmail.com>
2020-05-02 14:32:14 +0700

    betulinDatabase

--- aplikasi/conn.php
+++ aplikasi/conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "proyekaplin";
+$dbname = "onlineshop";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
