6b8a0bb55945979cfa2a3693b00c937ef3cfda9a
stefanie <stefanieangelina.sa@gmail.com>
2020-05-05 15:15:17 +0700

    Revert "Merge branch 'master' of https://github.com/Joms1026/OnlineShop"
    This reverts commit 443839bb5fbcf37ea686a26264a42a838b01436f, reversing
    changes made to c1d4745f672c9cd743a91f7aecffc93edbc74c47.

--- aplikasi/admin/page-part-admin/admin-side-nav.php
+++ aplikasi/admin/page-part-admin/admin-side-nav.php
@@ -14,7 +14,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-    <span class="brand-text font-weight-light">Admin Sunshop</span>
+    <img src="vendor-admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        style="opacity: .8">
+    <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
     <!-- Sidebar -->
@@ -25,7 +27,7 @@
         <img src="vendor-admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-        <a href="#" class="d-block">Admin Sunshop</a>
+        <a href="#" class="d-block">Alexander Pierce</a>
         </div>
     </div>
 
