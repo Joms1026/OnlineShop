322437da2da14cc87389614008f00c03742b47ee
stefanie <stefanieangelina.sa@gmail.com>
2020-05-11 22:27:16 +0700

    adminSementara

--- aplikasi/ajax/getAllProduct.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('conn.php');
-    // untuk select semua product yang ada dari database
-
-    $querySelect = "SELECT * FROM barang";
-    $returnValue = mysqli_query($conn, $querySelect)->fetch_all();
-
-    echo json_encode($returnValue);
-?>
