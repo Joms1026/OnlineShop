312a591bd6d18ba3bfd633485a964f72cda29304
stefanie <stefanieangelina.sa@gmail.com>
2020-05-11 22:28:51 +0700

    pindahFolder

--- aplikasi/admin/page-part-admin/admin-required-script.php
+++
@@ -1,8 +0,0 @@
-<!-- jQuery -->
-<script src="vendor-admin/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap -->
-<script src="vendor-admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="vendor-admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="vendor-admin/dist/js/adminlte.js"></script>
