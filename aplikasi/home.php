25806afbbb2ae1e4435bbb7d94f44388e27f9d4e
stefanie <stefanieangelina.sa@gmail.com>
2020-05-11 20:35:31 +0700

    kembalikan$user

--- aplikasi/home.php
+++ aplikasi/home.php
@@ -1,11 +1,11 @@
 <?php
 include("conn.php");
 session_start();
-$user="ste";
+$user=$_SESSION['username'];
 
-/*if (!isset($_SESSION['username'])) {
+if (!isset($_SESSION['username'])) {
 	header('location:index.php');
-}*/
+}
 if (isset($_POST['Logout'])) {
     header('location:index.php');
     unset($_SESSION['username']);
@@ -357,160 +357,6 @@ if (isset($_POST['Logout'])) {
 			</div>
 		</div>
 	</div>
-
-	<!-- Benefit -->
-
-	<!--<div class="benefit">
-		<div class="container">
-			<div class="row benefit_row">
-				<div class="col-lg-3 benefit_col">
-					<div class="benefit_item d-flex flex-row align-items-center">
-						<div class="benefit_icon"><i class="fa fa-truck" aria-hidden="true"></i></div>
-						<div class="benefit_content">
-							<h6>free shipping</h6>
-							<p>Suffered Alteration in Some Form</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 benefit_col">
-					<div class="benefit_item d-flex flex-row align-items-center">
-						<div class="benefit_icon"><i class="fa fa-money" aria-hidden="true"></i></div>
-						<div class="benefit_content">
-							<h6>cach on delivery</h6>
-							<p>The Internet Tend To Repeat</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 benefit_col">
-					<div class="benefit_item d-flex flex-row align-items-center">
-						<div class="benefit_icon"><i class="fa fa-undo" aria-hidden="true"></i></div>
-						<div class="benefit_content">
-							<h6>45 days return</h6>
-							<p>Making it Look Like Readable</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 benefit_col">
-					<div class="benefit_item d-flex flex-row align-items-center">
-						<div class="benefit_icon"><i class="fa fa-clock-o" aria-hidden="true"></i></div>
-						<div class="benefit_content">
-							<h6>opening all week</h6>
-							<p>8AM - 09PM</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>-->
-
-	<!-- Blogs -->
-
-	<!--<div class="blogs">
-		<div class="container">
-			<div class="row">
-				<div class="col text-center">
-					<div class="section_title">
-						<h2>Latest Blogs</h2>
-					</div>
-				</div>
-			</div>
-			<div class="row blogs_container">
-				<div class="product_slider_container">
-					<div class="owl-carousel owl-theme product_slider">
-						<div class="col-lg-4 blog_item_col">
-								<div class="blog_item">
-									<div class="blog_background" style="background-image:url(images/blog_jambi.jpg)"></div>
-									<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-										<h4 class="blog_title">Batik Jambi</h4>
-										<span class="blog_meta">by Giovanno Battista | dec 10, 2019</span>
-										<a class="blog_more" href="#">Read more</a>
-									</div>
-								</div>			
-						</div>
-						<div class="col-lg-4 blog_item_col">
-								<div class="blog_item">
-									<div class="blog_background" style="background-image:url(images/blog_madura.jpg)"></div>
-									<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-										<h4 class="blog_title">Batik Madura</h4>
-										<span class="blog_meta">by Giovanno Battista | dec 10, 2019</span>
-										<a class="blog_more" href="#">Read more</a>
-									</div>
-								</div>
-						</div>
-						<div class="col-lg-4 blog_item_col">
-								<div class="blog_item">
-									<div class="blog_background" style="background-image:url(images/blog_sumatra.jpg)"></div>
-									<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-										<h4 class="blog_title">Batik Sumatra</h4>
-										<span class="blog_meta">by Giovanno Battista | dec 10, 2019</span>
-										<a class="blog_more" href="#">Read more</a>
-									</div>
-								</div>
-						</div>
-						<div class="col-lg-4 blog_item_col">
-								<div class="blog_item">
-									<div class="blog_background" style="background-image:url(images/blog_sumatra.jpg)"></div>
-									<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-										<h4 class="blog_title">Batik Sumatra</h4>
-										<span class="blog_meta">by Giovanno Battista | dec 10, 2019</span>
-										<a class="blog_more" href="#">Read more</a>
-									</div>
-								</div>
-						</div>
-						<div class="col-lg-4 blog_item_col">
-							<div class="blog_item">
-								<div class="blog_background" style="background-image:url(images/blog_sumatra.jpg)"></div>
-								<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-									<h4 class="blog_title">Batik Sumatra</h4>
-									<span class="blog_meta">by Giovanno Battista | dec 10, 2019</span>
-									<a class="blog_more" href="#">Read more</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4 blog_item_col">
-							<div class="blog_item">
-								<div class="blog_background" style="background-image:url(images/blog_sumatra.jpg)"></div>
-								<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-									<h4 class="blog_title">Batik Sumatra</h4>
-									<span class="blog_meta">by Giovanno Battista | dec 10, 2019</span>
-									<a class="blog_more" href="#">Read more</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="product_slider_nav_left product_slider_nav d-flex align-items-center justify-content-center flex-column">
-						<i class="fa fa-chevron-left" aria-hidden="true"></i>
-					</div>
-					<div class="product_slider_nav_right product_slider_nav d-flex align-items-center justify-content-center flex-column">
-						<i class="fa fa-chevron-right" aria-hidden="true"></i>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>-->
-
-	<!-- Newsletter -->
-
-	<!--<div class="newsletter">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="newsletter_text d-flex flex-column justify-content-center align-items-lg-start align-items-md-center text-center">
-						<h4>Newsletter</h4>
-						<p>Subscribe to our newsletter and get 20% off your first purchase</p>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<form action="post">
-						<div class="newsletter_form d-flex flex-md-row flex-column flex-xs-column align-items-center justify-content-lg-end justify-content-center">
-							<input id="newsletter_email" type="email" placeholder="Your email" required="required" data-error="Valid email is required.">
-							<button id="newsletter_submit" type="submit" class="newsletter_submit_btn trans_300" value="Submit">subscribe</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>-->
 
 	<!-- Footer -->
 
