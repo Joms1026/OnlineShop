b25a211c72d945f6821ab37e4286fb211fdb93ce
stefanie <stefanieangelina.sa@gmail.com>
2020-05-15 11:17:45 +0700

    data4

--- aplikasi/admin/conn-admin.php
+++ aplikasi/admin/conn-admin.php
@@ -27,7 +27,6 @@ function executeQuery($query){
     // $sql = "SELECT Lastname, Age FROM Persons ORDER BY Lastname";
     $conn = connect();
     $result = $conn->query($query);
-    if($result)
     return $result->fetch_all(MYSQLI_ASSOC);
     // Fetch all
 }
