31e59e8d263e78b6ebad84b4cb63199df4cd2a00
adellaanwary55@gmail.com <adellaanwary55@gmail.com>
2020-05-19 00:03:44 +0700

    add tawk to to customer page

--- aplikasi/detail.php
+++ aplikasi/detail.php
@@ -560,3 +560,16 @@ if(isset($_GET['idx'])){
 		x[slideIndex[no]-1].style.display = "block"; 
 	}
 </script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ec2bb71967ae56c521aec98/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
