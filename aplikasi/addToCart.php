46aace91b9840637a2c8751b416f300d3c6f6de5
erlandego <50873837+erlandego@users.noreply.github.com>
2020-05-20 00:46:01 +0700

    a

--- aplikasi/addToCart.php
+++ aplikasi/addToCart.php
@@ -8,13 +8,13 @@
     $iduser = $_SESSION["userid"];
     $idbarang = $_POST["id"];
     $jumlahbarang = $_POST["count"];
-    $queryharga = "SELECT HARGA FROM VARIAN_BAJU WHERE ID_BAJU = '$idbarang'";
+    $queryharga = "SELECT HARGA FROM varian_baju WHERE ID_BAJU = '$idbarang'";
     $res2 = mysqli_query($conn , $queryharga);
     $hargabarang = 0;
     while ($row = mysqli_fetch_assoc($res2)) {
         $hargabarang = $row["HARGA"];
     }
-    $queryinsert = "INSERT INTO KERANJANG VALUES('',$iduser,$idbarang,$jumlahbarang,$hargabarang,'$ukr','$warna')";
+    $queryinsert = "INSERT INTO keranjang VALUES('',$iduser,$idbarang,$jumlahbarang,$hargabarang,'$ukr','$warna')";
     $ins = mysqli_query($conn , $queryinsert);
     echo "jalan";
 ?>
