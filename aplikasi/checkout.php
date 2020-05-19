a4e23fc6128776f3d1b26b6bd1bef04ccf7620b0
erlandego <50873837+erlandego@users.noreply.github.com>
2020-05-19 22:47:11 +0700

    Checkout

--- aplikasi/checkout.php
+++ aplikasi/checkout.php
@@ -190,25 +190,6 @@
                                 </div>
                             </div>
                             <hr class="mb-4">
-
-                            <h4 class="mb-3">Payment</h4>
-                            <div class="row" style="width: 465px">
-                                <div class="col-md-6 mb-3">
-                                    <label for="cc-name">Name on card</label>
-                                    <input type="text" class="form-control" id="cc-name" placeholder="" name="nameoncard" required>
-                                    <small class="text-muted">Full name as displayed on card</small>
-                                    <div class="invalid-feedback">
-                                        Name on card is required
-                                    </div>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="cc-number">Credit card number</label>
-                                    <input type="text" class="form-control" id="cc-number" placeholder="" name="ccnum" required>
-                                    <div class="invalid-feedback">
-                                        Credit card number is required
-                                    </div>
-                                </div>
-                            </div>
                             <hr class="mb-4">
                             <button class="btn btn-primary btn-lg btn-block" name="bayar" type="submit" style="width: 465px">
                                 <i class="fa fa-credit-card"></i> Continue to checkout</button>
